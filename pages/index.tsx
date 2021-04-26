10840e517b74651c86c6be941db29198c9d7ca5a
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-26 12:40:20 -0300

    Criando projeto

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.homeContainer}>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
