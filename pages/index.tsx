6bc9916a4bd23e787a0036058d42f654de0fc548
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-27 19:20:21 -0300

    Adicionando nova imagem para usar como Background do Header + finalizando parte de baixo do Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ export default function Home() {
         <div className={styles.homeContainer}>
             <section>
                 <div className={styles.bottom}>
-                    
+                    <div>
+                        <h1>Pizza Hut</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus.</p>
+                        <button type="button">Pedir agora</button>
+                    </div>
                 </div>
             </section>
         </div>
