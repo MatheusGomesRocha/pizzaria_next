bbbdca6e02ab439149507d1fba842fb6b6c71c8d
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-27 18:20:02 -0300

    Adicionando imagem SVG no Divider dos Forms + Criando page Auth que é onde vai ficar a tela com os Forms de Login e Register

---
+++ pages/auth/index.tsx
@@ -0,0 +1,24 @@
+import { useContext, useState } from 'react';
+import Form from '../../components/Form';
+import { FormContext } from '../../contexts/FormContext';
+
+import styles from './styles.module.scss';
+
+export default function Home() {
+  const [moveDiv, setMoveDiv] = useState(false);
+
+  const {showForm} = useContext(FormContext);
+
+  return (
+    <div className={styles.homeContainer}>
+
+      <Form register={true} moveDiv={moveDiv} setMoveDiv={setMoveDiv} />
+
+      <Form login={true} moveDiv={moveDiv} setMoveDiv={setMoveDiv}/>
+
+      <div className={styles.divider} id={showForm == 'login' ? styles.moveDivToLeft : styles.moveDivToRight} onClick={() => setMoveDiv(!moveDiv)}>
+        <img src="/auth.svg" />
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,9 @@
-import { useContext, useState } from 'react';
-import Form from '../components/Form';
-import { FormContext } from '../contexts/FormContext';
-
 import styles from './home.module.scss';
 
 export default function Home() {
-  const [moveDiv, setMoveDiv] = useState(false);
-
-  const {showForm} = useContext(FormContext);
-
-  return (
-    <div className={styles.homeContainer}>
-
-      <Form register={true} moveDiv={moveDiv} setMoveDiv={setMoveDiv} />
-
-      <Form login={true} moveDiv={moveDiv} setMoveDiv={setMoveDiv}/>
-
-      <div className={styles.divider} id={showForm == 'login' ? styles.moveDivToLeft : styles.moveDivToRight} onClick={() => setMoveDiv(!moveDiv)}>
-        <img src="/auth.svg" />
-      </div>
-    </div>
-  )
+    return(
+        <div className={styles.homeContainer}>
+            <h1>olá mundo</h1>
+        </div>
+    )
 }
