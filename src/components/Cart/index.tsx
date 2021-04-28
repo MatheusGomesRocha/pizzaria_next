3299bc6f44cb679a76924b7dc19f747883837bf2
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-28 19:35:40 -0300

    Finalizando estilização do Cart quando não possuir produtos

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -3,6 +3,25 @@ import styles from './styles.module.scss';
 export default function Cart() {
     return(
         <div className={styles.cartContainer}>
+            <section>
+                <h1>Carrinho</h1>
+
+                <p>Seu carrinho está vazio</p>
+
+                <div>
+                    <span>Taxa de entrega</span>
+                    <span>R$ 0,00</span>
+                </div>
+
+                <div className={styles.cupomCode}>
+                    <span>Tem um código de cupom?</span>
+                </div>
+            </section>
+
+
+            <div className={styles.buttonArea}>
+                <button type="button">Finalizar pedido</button>
+            </div>
 
         </div>
     )
