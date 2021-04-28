ffa65efcd2db0c113464b67905caacf34eba5b51
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-28 09:58:48 -0300

    Mudando completamente a página inicial, agora imitando o site do Pizza Hut + fazendo até a parte dos buttons do formulário de forma de entrega

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
+import Cart from '../components/Cart'
 import { CartContextProvider } from '../contexts/CartContext'
 import { FormContextProvider } from '../contexts/FormContext'
 import '../styles/globals.scss'
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <CartContextProvider>
       <FormContextProvider>
-        <Component {...pageProps} />
+        <div className={styles.wrapper}>
+          {/* <Cart /> */}
+
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </div>
       </FormContextProvider>
     </CartContextProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,55 @@
+import { useState } from 'react';
+import { FaUserCircle, FaMotorcycle } from 'react-icons/fa';
+import  { IoIosRestaurant } from 'react-icons/io';
+
 import styles from './home.module.scss';
 
 export default function Home() {
+    const[whatMode, setWhatMode] = useState('delivery');
+
     return(
         <div className={styles.homeContainer}>
             <section>
                 <nav className={styles.topNavigator}>
-                    <span>Logo Aqui</span>
+                    <h2>Logo Aqui</h2>
 
-                    <div>
-                        <a href="#">Home</a>
-                        <a href="#">Products</a>
-                        <a href="#">Contact</a>
+                    <div className={styles.goToAuth}>
+                        <div>
+                            <FaUserCircle color="#333" size={25} />
+                            <span>Fazer login</span>
+                        </div>
+                        <div>
+                            <FaUserCircle color="#333" size={25} />
+                            <span>Criar conta</span>
+                        </div>
                     </div>
                 </nav>
 
-                <div className={styles.bottom}>
-                    <div>
-                        <h1>Pizza Hut</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus.</p>
-                        <button type="button">Pedir agora</button>
+                <div className={styles.bottomForm}>
+
+                    <div className={styles.buttonArea}>
+                        <button className={whatMode == 'delivery' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('delivery')}>
+                            <div>
+                                <FaMotorcycle color={whatMode === 'delivery' ? '#fe705f' : '#808080'} size={25} />
+                            </div>
+                            <span>Delivery</span>
+                        </button>
+
+                        <button className={whatMode == 'restaurant' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('restaurant')}>
+                            <div>
+                                <IoIosRestaurant color={whatMode === 'restaurant' ? '#fe705f' : '#808080'} size={25} />
+                            </div>
+                            <span>Retirada</span>
+                        </button>
                     </div>
+
+                    <div className={styles.formArea}>
+                        
+                    </div>
+
                 </div>
+
+             
             </section>
         </div>
     )
