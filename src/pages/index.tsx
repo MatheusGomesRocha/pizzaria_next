fbf82e8ff37ce24949c3ed117983f3e0c11315fb
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-28 11:55:09 -0300

    Finalizando Form que o usuário escolhe o modo de entrega do pedido, entre Delivery ou Retirar no Restaurante

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FaUserCircle, FaMotorcycle } from 'react-icons/fa';
 import  { IoIosRestaurant } from 'react-icons/io';
+import  { RiSendPlaneLine } from 'react-icons/ri';
 
 import styles from './home.module.scss';
 
@@ -28,14 +29,14 @@ export default function Home() {
                 <div className={styles.bottomForm}>
 
                     <div className={styles.buttonArea}>
-                        <button className={whatMode == 'delivery' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('delivery')}>
+                        <button className={whatMode === 'delivery' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('delivery')}>
                             <div>
                                 <FaMotorcycle color={whatMode === 'delivery' ? '#fe705f' : '#808080'} size={25} />
                             </div>
                             <span>Delivery</span>
                         </button>
 
-                        <button className={whatMode == 'restaurant' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('restaurant')}>
+                        <button className={whatMode === 'restaurant' ? styles.buttonClicked : undefined} type="button" onClick={() => setWhatMode('restaurant')}>
                             <div>
                                 <IoIosRestaurant color={whatMode === 'restaurant' ? '#fe705f' : '#808080'} size={25} />
                             </div>
@@ -44,7 +45,23 @@ export default function Home() {
                     </div>
 
                     <div className={styles.formArea}>
-                        
+                        <h2>Deliverys sem contato</h2>
+                        <p>Para garantir a segurança de nossos clientes e entregadores, todas as nossas entregas serão realizadas sem contato. <a href="">Leia Mais</a></p>
+
+                        <span>{whatMode === 'delivery' ? 'Onde você quer a sua pizza?' : 'Digite sua localização para encontrar uma Pizza Hut nas proximidades'}</span>
+
+                        <h3>Insira o seu CEP e número para encontrarmos a loja mais próxima de você.</h3>
+
+                        <form>
+                            <input type="text" placeholder={whatMode === 'delivery' ? 'CEP' : 'CEP, endereço ou cidade'} />
+                            {whatMode === 'delivery' ? <input type="text" placeholder="Insiria o número da casa/prédio" /> : undefined}
+                            <button type="submit">Buscar</button>
+
+                            <div>
+                                <RiSendPlaneLine color="#fe705f" size={20} />
+                                <span>Usar minha localização atual?</span>
+                            </div>
+                        </form>
                     </div>
 
                 </div>
